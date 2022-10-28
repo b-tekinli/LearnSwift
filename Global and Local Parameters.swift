1a41119beef4563cd734ec691796d515ddeac5d2
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2022-10-28 15:28:09 +0300

    added

---
+++ Global and Local Parameters.swift
@@ -0,0 +1,7 @@
+func calculate (number1 n1:Int, number2 n2:Int) -> Int {
+    let result = n1 * 2 + n2 * 4
+    return result
+}
+
+var c = calculate(number1: 30, number2: 40)
+print("Result = \(c)")
