5d96cb1aa0282e06750f1b3e2f1c07597b8cd80b
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2022-11-01 22:25:27 +0300

    added

--- Array Example.swift
+++ Array Example.swift
@@ -1,22 +1,25 @@
 class LessonNotes {
-    var lesName:Int?
-    var lesNote:String?
+    var lesName:String?
+    var lesNote:Int?
 
     init(lesName:String, lesNote:Int) {
-        self.name = lesName
-        self.no = lesNote
+        self.lesName = lesName
+        self.lesNote = lesNote
     }
 }
 
 let l1 = LessonNotes(lesName: "Mat", lesNote: 90)
 let l2 = LessonNotes(lesName: "Kimya", lesNote: 100)
 let l3 = LessonNotes(lesName: "Biyoloji", lesNote: 95)
+let l4 = LessonNotes(lesName: "Fizik", lesNote: 75)
+
 
 var lesList = [LessonNotes]()
 
 lesList.append(l1)
 lesList.append(l2)
 lesList.append(l3)
+lesList.append(l4)
 
 var sum = 0
 
@@ -25,4 +28,12 @@ for ls in lesList {
     sum += ls.lesNote!
 }
 
-print("Ortalama: \(sum / lesList.count) ")
+let average = sum / lesList.count
+print("\nOrtalama: \(average)")
+
+if average >= 50 {
+    print("Geçti")
+}
+else {
+    print("Kaldı")
+}
