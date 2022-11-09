48a512404102e6ca9e1ba8842baf4fdb2783456a
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2022-11-09 16:59:56 +0300

    added

---
+++ İlhamVer/İlhamVer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  İlhamVer
+//
+//  Created by Beyzanur Tekinli on 11/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
