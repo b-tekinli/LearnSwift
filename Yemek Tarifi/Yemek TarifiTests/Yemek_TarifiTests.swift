dcb01417651ed63e2ddd7871a654c855e0b5c441
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2022-11-09 17:43:49 +0300

    added

---
+++ Yemek Tarifi/Yemek TarifiTests/Yemek_TarifiTests.swift
@@ -0,0 +1,33 @@
+//
+//  Yemek_TarifiTests.swift
+//  Yemek TarifiTests
+//
+//  Created by Beyzanur Tekinli on 11/9/22.
+//
+
+import XCTest
+@testable import Yemek_Tarifi
+
+class Yemek_TarifiTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
