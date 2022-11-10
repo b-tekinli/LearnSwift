0110e2113604be6e80ca961715ad0aba62e6ba83
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2022-11-10 16:02:29 +0300

    added

---
+++ Dissmiss Metodu/Present Modally App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Present Modally App
+//
+//  Created by Beyzanur Tekinli on 11/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Dissmiss Metodu/Present Modally App/ViewController2.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController2.swift
+//  Present Modally App
+//
+//  Created by Beyzanur Tekinli on 11/10/22.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    @IBAction func tiklaDismiss(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
