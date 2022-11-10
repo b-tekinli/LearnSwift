b2dafbf925b35e26030e2cfba6ed320b9f3971fd
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2022-11-10 15:41:17 +0300

    added

---
+++ Present Modally App/Present Modally App/ViewController3.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController3.swift
+//  Present Modally App
+//
+//  Created by Beyzanur Tekinli on 11/10/22.
+//
+
+import UIKit
+
+class ViewController3: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
